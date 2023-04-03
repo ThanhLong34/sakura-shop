dd2197390239ee443eeadd935d2aa863e17003b2
DragonDev <thanhlongedu0304@gmail.com>
2023-04-03 13:41:31 +0700

    Install primereact

--- src/app/src/App.jsx
+++ src/app/src/App.jsx
@@ -1,9 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserView, MobileView, isBrowser, isMobile } from "react-device-detect";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Welcome</h1>
+			<BrowserView>
+				<h1>This is rendered only in browser</h1>
+			</BrowserView>
+			<MobileView>
+				<h1>This is rendered only on mobile</h1>
+			</MobileView>
 		</div>
 	);
 }
