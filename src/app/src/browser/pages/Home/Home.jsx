9a2b81d3e702ee785ea24e5578c1cb97d295a2ca
DragonDev <thanhlongedu0304@gmail.com>
2023-04-20 10:02:44 +0700

    Update home UI

--- src/app/src/browser/pages/Home/Home.jsx
+++ src/app/src/browser/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
+
+import giftApi from "@/apis/giftApi";
 
 import Logo from "@/assets/images/LogoX250.png";
 import GameTitle from "@/assets/images/GameTitle.png";
@@ -13,15 +15,26 @@ const cx = classNames.bind(styles);
 
 function Home() {
 	const [action, setAction] = useState(0);
+	const [gifts, setGifts] = useState([]);
+
+	useEffect(() => {
+		giftApi.getAll({ limit: 6, offset: 0, fillType: "isSpecial", fillValue: 1 }).then((response) =>
+			setGifts(
+				response.data.map((gift) => ({
+					...gift,
+					id: +gift.id,
+					isSpecial: +gift.isSpecial === 1,
+				}))
+			)
+		);
+	}, []);
 
 	const handleGoBack = useCallback(() => {
 		setAction(null);
 	}, []);
-
 	const handleShowRegisterForm = useCallback(() => {
 		setAction("ShowRegisterForm");
 	}, []);
-
 	const handleShowLoginForm = useCallback(() => {
 		setAction("ShowLoginForm");
 	}, []);
@@ -46,30 +59,7 @@ function Home() {
 					<div className="zoomin animation-duration-500 animation-iteration-1 animation-ease-out">
 						<h3 className={cx("title")}>PHẦN THƯỞNG HẤP DẪN</h3>
 						<div className={cx("image-wrapper")}>
-							<img
-								src="https://product.hstatic.net/1000025647/product/029_dior-min_56812b8e527f457488371480c0cfaa93_1024x1024.jpg"
-								alt="gift image"
-							/>
-							<img
-								src="https://product.hstatic.net/1000025647/product/giftset_gucci_flora_gorgeous_jasmine_edp___100ml___10ml___5ml_-min_c2d4301942d24cd1a67698c9a319b2a3_1024x1024.jpg"
-								alt="gift image"
-							/>
-							<img
-								src="https://product.hstatic.net/1000025647/product/1_e2f5c4830778465eac4c871582ee6a92_large_f1702ea67a2d43b5ba3df7e0f9e0aa81_1024x1024.jpg"
-								alt="gift image"
-							/>
-							<img
-								src="https://product.hstatic.net/1000025647/product/ior-rouge-mitzah-velvet-999__2__6b0f64a40b534f978c39c91a6b6ca8d7_large_2f729541d84f4772b7030876aebad614_1024x1024.png"
-								alt="gift image"
-							/>
-							<img
-								src="https://product.hstatic.net/1000025647/product/nuoc_hoa_ysl_black_opium_le_parfum_50ml__-_nong_nang__goi_cam__2__eb867434f8904198a78fe31f50cd817e_1024x1024.jpg"
-								alt="gift image"
-							/>
-							<img
-								src="https://product.hstatic.net/1000025647/product/bleu_de_chanel_edt_150ml_aa05e910eece486299be921d45ffed57_1024x1024.png"
-								alt="gift image"
-							/>
+							{gifts && gifts.map((gift) => <img key={gift.id} src={gift.imageUrl} alt="gift" />)}
 						</div>
 						<h3 className={cx("desc")}>
 							Còn chần chừ gì nữa, <br /> nhanh tay chơi game săn quà nào bạn ơi!
