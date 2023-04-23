885710afa3ac4c6fe1d01fef3ebe4f9379798aa7
DragonDev <thanhlongedu0304@gmail.com>
2023-04-23 15:36:40 +0700

    Done OptionalMode

--- src/app/src/browser/components/SelectLevel/SelectLevel.jsx
+++ src/app/src/browser/components/SelectLevel/SelectLevel.jsx
@@ -9,14 +9,22 @@ import NormalLevelImage from "@/assets/images/levels/NormalLevel.png";
 import HardLevelImage from "@/assets/images/levels/HardLevel.png";
 
 import { gameConvention } from "@/constant";
+import IconButton from "../IconButton";
 
 const cx = classNames.bind(styles);
 
 SelectLevel.propTypes = {
 	topicId: PropTypes.number.isRequired,
+	showGoBackButton: PropTypes.bool,
+	onGoBack: PropTypes.func,
 };
 
-function SelectLevel({ topicId }) {
+SelectLevel.defaultProps = {
+	showGoBackButton: false,
+	onGoBack: () => {},
+};
+
+function SelectLevel({ topicId, showGoBackButton, onGoBack }) {
 	const navigate = useNavigate();
 
 	const handlePlayGame = (selectedLevel) => {
@@ -27,18 +35,28 @@ function SelectLevel({ topicId }) {
 		<>
 			<div className="grid">
 				<div className="col-7 col-offset-2">
-					<h5 className={cx("heading", "mt-3 mb-3")}>CHỌN CẤP ĐỘ CHƠI</h5>
+					<div className="flex align-items-center justify-content-center">
+						{showGoBackButton && (
+							<IconButton className={cx("go-back-btn")} icon="pi pi-chevron-left" onClick={onGoBack} />
+						)}
+						<h5 className={cx("heading", "mt-3 mb-3")}>CHỌN CẤP ĐỘ CHƠI</h5>
+					</div>
 				</div>
 			</div>
 			<div className="grid">
 				<div className="col-1"></div>
 				<div className="col-3">
-					<div className={cx("level-item", "card")} onClick={() => handlePlayGame(gameConvention.levels.easy.name)}>
+					<div
+						className={cx("level-item", "card")}
+						onClick={() => handlePlayGame(gameConvention.levels.easy.name)}
+					>
 						<div className={cx("level-item-image")}>
 							<img src={EasyLevelImage} alt="easy level" />
 						</div>
 						<div className={cx("level-item-heading")}>Dễ</div>
-						<div className={ cx("level-item-quantity-card") }>{gameConvention.levels.easy.quantityCard} thẻ bài</div>
+						<div className={cx("level-item-quantity-card")}>
+							{gameConvention.levels.easy.quantityCard} thẻ bài
+						</div>
 						<div className={cx("level-item-grid")}>
 							{new Array(gameConvention.levels.easy.quantityCard).fill(null).map((item, idx) => (
 								<div key={idx} className={cx("level-item-grid-item")}></div>
@@ -47,12 +65,17 @@ function SelectLevel({ topicId }) {
 					</div>
 				</div>
 				<div className="col-3">
-					<div className={cx("level-item", "card")} onClick={() => handlePlayGame(gameConvention.levels.normal.name)}>
+					<div
+						className={cx("level-item", "card")}
+						onClick={() => handlePlayGame(gameConvention.levels.normal.name)}
+					>
 						<div className={cx("level-item-image")}>
 							<img src={NormalLevelImage} alt="easy level" />
 						</div>
 						<div className={cx("level-item-heading")}>Trung bình</div>
-						<div className={ cx("level-item-quantity-card") }>{gameConvention.levels.normal.quantityCard} thẻ bài</div>
+						<div className={cx("level-item-quantity-card")}>
+							{gameConvention.levels.normal.quantityCard} thẻ bài
+						</div>
 						<div className={cx("level-item-grid")}>
 							{new Array(gameConvention.levels.normal.quantityCard).fill(null).map((item, idx) => (
 								<div key={idx} className={cx("level-item-grid-item")}></div>
@@ -61,12 +84,17 @@ function SelectLevel({ topicId }) {
 					</div>
 				</div>
 				<div className="col-3">
-					<div className={cx("level-item", "card")} onClick={() => handlePlayGame(gameConvention.levels.hard.name)}>
+					<div
+						className={cx("level-item", "card")}
+						onClick={() => handlePlayGame(gameConvention.levels.hard.name)}
+					>
 						<div className={cx("level-item-image")}>
 							<img src={HardLevelImage} alt="easy level" />
 						</div>
 						<div className={cx("level-item-heading")}>Khó</div>
-						<div className={ cx("level-item-quantity-card") }>{gameConvention.levels.hard.quantityCard} thẻ bài</div>
+						<div className={cx("level-item-quantity-card")}>
+							{gameConvention.levels.hard.quantityCard} thẻ bài
+						</div>
 						<div className={cx("level-item-grid", "grid-col-5")}>
 							{new Array(gameConvention.levels.hard.quantityCard).fill(null).map((item, idx) => (
 								<div key={idx} className={cx("level-item-grid-item")}></div>
--- src/app/src/browser/pages/OptionalMode/OptionalMode.jsx
+++ src/app/src/browser/pages/OptionalMode/OptionalMode.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import classNames from "classnames/bind";
 import styles from "./OptionalMode.module.scss";
+
 import NotAllowedToPlay from "@/browser/components/NotAllowedToPlay";
 import LockedMode from "@/browser/components/LockedMode";
+import SelectLevel from "@/browser/components/SelectLevel";
+import topicApi from "@/apis/topicApi";
 
 const cx = classNames.bind(styles);
 
 function OptionalMode() {
 	const playerAccount = useSelector((state) => state.player.account);
 
+	const [topics, setTopics] = useState(null);
+	const [topicSelected, setTopicSelected] = useState(null);
+
 	const levelRequire = 10;
+
+	useEffect(() => {
+		topicApi.getAll().then((response) => {
+			if (response.code === 1) {
+				setTopics(
+					response.data.map((topic) => ({
+						...topic,
+						id: +topic.id,
+					}))
+				);
+			}
+		});
+	}, []);
+
+	const handleResetTopicSelected = () => {
+		setTopicSelected(null);
+	};
 
 	if (playerAccount.level < levelRequire) {
 		return (
@@ -20,7 +44,39 @@ function OptionalMode() {
 	}
 
 	if (playerAccount.health > 0) {
-		return <div>OptionalMode</div>;
+		if (topicSelected) {
+			return (
+				<div>
+					<SelectLevel topicId={topicSelected} showGoBackButton onGoBack={handleResetTopicSelected} />;
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<h5 className={cx("heading", "mt-3 mb-5")}>CHỌN CHỦ ĐỀ</h5>
+					<div
+						className="grid"
+						style={{
+							justifyContent: "center",
+						}}
+					>
+						{topics &&
+							topics
+								.filter((topic) => topic.id !== 1)
+								.map((topic) => (
+									<div key={topic.id} className="col-3">
+										<div className={cx("card", "topic")} onClick={() => setTopicSelected(topic.id)}>
+											<img className={cx("topic-image")} src={topic.imageUrl} alt="topic" />
+											<div className={cx("topic-info")}>
+												<div className={cx("topic-name")}>{topic.name}</div>
+											</div>
+										</div>
+									</div>
+								))}
+					</div>
+				</div>
+			);
+		}
 	} else {
 		return (
 			<div>
